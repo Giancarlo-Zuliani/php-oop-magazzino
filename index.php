9ee1c3d5284af30eb58e913ea52c64e1c94ad126
Giancarlo Zuliani <slaigox@gmail.com>
2021-01-15 17:35:57 +0100

    minor fix and code clean

--- index.php
+++ index.php
@@ -6,53 +6,56 @@
     <link rel="stylesheet" href="style.css">
   </head>
   <body>
-    <h1><?php echo "connesso" ?></h1>
 
     <?php
-
-    class Product{
-      public $name;
-      public $weight;
-      public $price;
-      function __construct($name,$weight,$price){
-        $this -> name = $name;
-        $this -> weight = $weight;
-        $this -> price = $price;
-      }
-    }
-
-    class Storage{
-      public $name;
-      public $adress;
-      public $products;
-      function __construct($name, $adress , $arr = []){
-        $this -> name = $name;
-        $this -> adress = $adress;
-        if (count($arr != 0)) {
-          $this -> products = $arr;
-        }else {
-          $this -> products = [];
+      class Product{
+        public $name;
+        public $weight;
+        public $price;
+        function __construct($name,$weight,$price){
+          $this -> name = $name;
+          $this -> weight = $weight;
+          $this -> price = $price;
         }
       }
-    }
 
-    $proarr = [];
-    $emptyarr = [];
+      class Storage{
+        public $name;
+        public $adress;
+        public $products;
+        function __construct($name, $adress , $arr = []){
+          $this -> name = $name;
+          $this -> adress = $adress;
+          if (count($arr != 0)) {
+            $this -> products = $arr;
+          }else {
+            $this -> products = [];
+          }
+        }
+        public function printProducts(){
+          echo "<h2>" . $this -> name . "</h2>";
+          echo "<h5>" . $this -> adress . "</h5>";
+          foreach ( $this -> products as $key =>  $value) {
+            echo "<ul><li>" . $value -> name . " weight : " . $value -> weight . " price : " . $value -> price . "</li></ul>";
+          }
+        }
+      }
 
-    $pro1 = new Product ('culo' , 3 ,150);
-    $pro2 = new Product ('gino' , 4 ,1502);
-    $pro3 = new Product ('caio' , 4 , 45454);
+      $proarr = [];
 
-    array_push($proarr , $pro1, $pro2 ,$pro3);
+      $pro1 = new Product ('box' , 3 , "3EUR");
+      $pro2 = new Product ('paper' , 0.4 ,"5EUR");
+      $pro3 = new Product ('pen' , 0.1 , "15EUR" );
 
-    $ferramenta = new Storage('ferramenta' , 'via e man dal cul' );
+      array_push($proarr , $pro1, $pro2 ,$pro3);
 
+      $ferramenta = new Storage('magazzino' , 'via San Carlo 30' , $proarr);
 
+    ?>
 
-    var_dump($ferramenta);
-
-     ?>
-
+     <?php
+       $ferramenta-> printProducts();
+      ?>
 
   </body>
 </html>
